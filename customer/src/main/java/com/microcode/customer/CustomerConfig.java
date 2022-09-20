bf3fccfa7268b23a3ad6d7d3ae370082e36f3a09
wael1998 <wael.chakchouk@esprit.tn>
2022-09-20 14:56:42 +0100

    test loadbalancer

--- customer/src/main/java/com/microcode/customer/CustomerConfig.java
+++ customer/src/main/java/com/microcode/customer/CustomerConfig.java
@@ -1,11 +1,13 @@
 package com.microcode.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CustomerConfig {
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
